d8d4cec87a1dd3f3e0edc25a80bf63f74c7a4c15
Kane Lee <kane.m.lee@gmail.com>
2017-01-31 10:16:10 -0500

    Add link to page to buy shares

--- app/assets/javascripts/components/Stock.es6.jsx
+++ app/assets/javascripts/components/Stock.es6.jsx
@@ -5,16 +5,16 @@ class Stock extends React.Component {
 
 
   render() {
+    let showStock='stocks/'+ this.props.symbol
     return(
       <li>
         <h2>{this.props.symbol}</h2>
-        <h3>{this.props.name}</h3>
+        <span><h3>{this.props.name}</h3></span>
         <span>{this.props.change} | </span>
         <span>{this.props.high} | </span>
         <span>{this.props.low} | </span>
-        <span>${this.props.current}</span>
-        <span>{link_to "add", stocks}</span>
-
+        <span>${this.props.current} | </span>
+        <span><a href={showStock}>add</a></span>
       </li>
     )
   }
