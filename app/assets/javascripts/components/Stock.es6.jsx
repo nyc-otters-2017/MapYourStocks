d42f2e9f9301461b281496adf5e4aaca9ea70b8a
[01;31m[K"name"[m[K: "kevin hu <[01;31m[K"email"[m[K: null>
2017-01-31 16:04:27 -0500

    styling

--- app/assets/javascripts/components/Stock.es6.jsx
+++ app/assets/javascripts/components/Stock.es6.jsx
@@ -6,14 +6,25 @@ class Stock extends React.Component {
 
   render() {
     let showStock='stocks/'+ this.props.symbol
+
     return(
       <li>
         <h2>{this.props.symbol}</h2>
         <span><h3>{this.props.name}</h3></span>
-        <span>{this.props.change} | </span>
-        <span>{this.props.high} | </span>
-        <span>{this.props.low} | </span>
-        <span>${this.props.current} | </span>
+        <table >
+          <tr>
+            <td> Change </td>
+            <td> High </td>
+            <td> Low </td>
+            <td> Current </td>
+          </tr>
+          <tr>
+            <td> {this.props.change} </td>
+            <td> {this.props.high} </td>
+            <td> {this.props.low} </td>
+            <td> {this.props.current} </td>
+          </tr>
+        </table>
         <span><a href={showStock}>add</a></span>
       </li>
     )
