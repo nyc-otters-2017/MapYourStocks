32a0769d38ac6ebfba2d2f0a074e6012f6a6007c
Kane Lee <kane.m.lee@gmail.com>
2017-01-31 12:22:48 -0500

    StockList auto refreshes

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -7,18 +7,26 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    // let url =       "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22"+ {ticker_tag} + "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+    this.callStockList()
+  }
 
+  callStockList() {
     $.ajax({
       url: 'https://query.yahooapis.com/v1/public/yql?q=select%20* from yahoo.finance.quotes where symbol in ("YHOO,AAPL,GOOG,MSFT,FB,INTC,AMZN,CTSH,CMCSA,AAL,AVGO,TMUS,NVDA,QCOM,PYPL,TSLA")&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
-
     }).success(function(response) {
       let stockList = response.query.results.quote
-
       this.setState({stocks: stockList})
       console.log(this.state.stocks)
     }.bind(this))
   }
+
+  componentDidMount() {
+    var refresh = setInterval(() => {
+      this.callStockList()
+    }, 10000);
+
+  }
+
 
 
   render() {
