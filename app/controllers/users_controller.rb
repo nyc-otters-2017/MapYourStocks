0755f85cae06e08de3f777c4826a03ab34de9371
Kane Lee <kane.m.lee@gmail.com>
2017-01-31 17:30:36 -0500

    Users can add stocks to their portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,11 @@ class UsersController < ApplicationController
   end
 
   def all
-    debugger
     @userStocks = current_user.portfolios.find(:id).stocks
-    url = ''
+    url = 'https://query.yahooapis.com/v1/public/yql?q=select%20* from yahoo.finance.quotes where symbol in (' + @userStocks + ')&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
     uri = URI(url)
     response = Net::HTTP.get(uri)
     render json: JSON.parse(response)
   end
+
 end
--- db/migrate/20170130152835_create_stocks.rb
+++ db/migrate/20170130152835_create_stocks.rb
@@ -2,8 +2,8 @@ class CreateStocks < ActiveRecord::Migration[5.0]
   def change
     create_table :stocks do |t|
       t.string :ticker, null: false
-      t.integer :buy, null: false #buy is buy-in price
-      t.integer :shares, null: false
+      t.string :buy, null: false #buy is buy-in price
+      t.string :shares, null: false
       t.references :portfolio, null: false
 
       t.timestamps
